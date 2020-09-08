05129d94fdff6efe08cd3122c10f04f21adab83f
prats7 <pratyushvishal04@gmail.com>
2020-09-08 19:46:38 +0530

    Add Total Price

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,40 @@
 import React from 'react';
 
-class Navbar extends React.Component {
-
-    render() {
-       
-        return (
-            <div>
-                <div>
-                    <img src="https://image.flaticon.com/icons/svg/833/833314.svg" alt="cart-icon"/>
-                </div>
-            </div>
-        );
-    }
+const Navbar = (props) => {
+  return (
+    <div style={styles.nav}>
+      <div style={styles.cartIconContainer}>
+        <img style={styles.cartIcon} src="https://image.flaticon.com/icons/svg/2121/2121815.svg" alt="cart-icon" />
+        <span style={styles.cartCount}> {props.count} </span>
+      </div>
+    </div>
+  );
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+  cartIcon: {
+    height: 32,
+    marginRight: 20
+  },
+  nav: {
+    height: 70,
+    background: '#4267b2',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  cartIconContainer: {
+    position: 'relative'
+  },
+  cartCount: {
+    background: 'yellow',
+    borderRadius: '50%',
+    padding: '4px 8px',
+    position: 'absolute',
+    right: 0,
+    top: -9
+  }
+};
+
 
 export default Navbar;
