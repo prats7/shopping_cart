66f43dfa13f4cf7b3b596c0c2b9641712ec5d770
prats7 <pratyushvishal04@gmail.com>
2020-09-10 13:53:54 +0530

    Add Firebase db

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBGAe4XPF0jDCMiNi3t_W9LjnGtES0sjWk",
+    authDomain: "cart-9d0c6.firebaseapp.com",
+    databaseURL: "https://cart-9d0c6.firebaseio.com",
+    projectId: "cart-9d0c6",
+    storageBucket: "cart-9d0c6.appspot.com",
+    messagingSenderId: "1008047303441",
+    appId: "1:1008047303441:web:e302ab8ed5937022fafab0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
